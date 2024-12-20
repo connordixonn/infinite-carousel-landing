771df651e5b9f195e79eee894fc5c97b2de6f298
connordixonn <connor@scaleyourleads.com>
2024-12-20 01:22:17 -0500

    a

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 left-0 right-0 bg-white z-50 shadow-sm h-16">
-      <div className="w-full pl-2.5">
-        <div className="flex items-center h-16">
-          <img 
-            src="https://res.cloudinary.com/dqojf0xy1/image/upload/v1734675094/SYL_website_ready_suu7uf.png"
-            alt="Scale Your Leads Logo"
-            style={{ 
-              width: '250px',
-              height: 'auto',
-              objectFit: 'contain'
-            }}
-          />
-        </div>
+    <header className="sticky top-0 left-0 right-0 bg-white z-[1000] shadow-sm h-16">
+      <div className="absolute left-2.5 h-full flex items-center">
+        <img 
+          src="https://res.cloudinary.com/dqojf0xy1/image/upload/v1734675094/SYL_website_ready_suu7uf.png"
+          alt="Scale Your Leads Logo"
+          style={{ 
+            width: '250px',
+            height: 'auto',
+            objectFit: 'contain'
+          }}
+          className="z-[1001]"
+        />
       </div>
     </header>
   );
