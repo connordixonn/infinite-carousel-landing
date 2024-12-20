0115f859c9d3bb4f9bee2c450675df639e817a56
connordixonn <connor@scaleyourleads.com>
2024-12-20 01:17:50 -0500

    a

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 bg-white z-50 shadow-sm h-16">
-      <div className="max-w-7xl mx-auto px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-3 sm:px-4">
         <div className="flex items-center h-16">
           <img 
             src="https://res.cloudinary.com/dqojf0xy1/image/upload/v1734675094/SYL_website_ready_suu7uf.png"
