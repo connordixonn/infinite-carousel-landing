3ef631f4e50d61cf593d88fc0f50268013c1518e
connordixonn <connor@scaleyourleads.com>
2024-12-20 01:05:13 -0500

    a

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ export const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 bg-white z-50 shadow-sm">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        <div className="flex items-center justify-between h-24">
+        <div className="flex items-center justify-between h-32">
           <img 
             src="https://res.cloudinary.com/dqojf0xy1/image/upload/v1734674606/Untitled_design_jje1m6.png"
             alt="Scale Your Leads Logo"
-            className="h-16 object-contain"
+            className="h-24 object-contain"
           />
         </div>
       </div>
